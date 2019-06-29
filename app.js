ffb1ce7279bb279a1b595e4c8fc0c10ada236fa1
Filip <wojtas_f@outlook.com>
2019-06-29 20:46:42 +0200

    blocked by CORS2

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ let lastPage;
 
 
         function searchForResults(searchString, page = 0) {
-            fetch(`https://itunes.apple.com/search?term=${searchString}&entity=song&limit=200`,{method: 'GET'})
+            fetch(`https://itunes.apple.com/search?term=${searchString}&entity=song&limit=200`,{method: 'GET',credentials: 'include',mode: 'cors',headers:{ 'Content-Type': 'application/json'} })
                 .then((data) => {
                     return data.json()
                 })
